4d98b070e752d858bd19cebaa83b06fd8e09ffdb
username <user@email.mail>
2021-10-10 20:52:55 +0900

    BJ1987

--- BAEKJOON/BJ1987_알파벳_dfs.py
+++ BAEKJOON/BJ1987_알파벳_dfs.py
@@ -7,7 +7,8 @@ g=[list(input().strip()) for _ in range(r)]
 result = 1
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
-visited_al= [g[0][0]]
+ch = [0] * 26
+
 def dfs(x,y,count):
     global result
     result = max(result,count)
@@ -16,10 +17,10 @@ def dfs(x,y,count):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if (0<=nx<r) and (0<=ny<c) and(g[nx][ny] not in visited_al):
-            visited_al.append(g[nx][ny])
+        if (0<=nx<r) and (0<=ny<c) and ch[ord(g[nx][ny])-ord('A')]==0:
+            ch[ord(g[nx][ny])-ord('A')] = 1
             dfs(nx,ny,count+1)
-            visited_al.remove(g[nx][ny])
-
+            ch[ord(g[nx][ny])-ord('A')] = 0
+ch[ord(g[0][0])-ord('A')]=1
 dfs(0,0,result)
 print(result)
