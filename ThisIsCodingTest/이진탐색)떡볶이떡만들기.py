d5a197987d9fcb8e98282b4b22d358730b162d13
박미희 <mihee78952@naver.com>
2021-10-11 19:06:29 +0900

    ThisIsCodingTest

--- ThisIsCodingTest/이진탐색)떡볶이떡만들기.py
+++ ThisIsCodingTest/이진탐색)떡볶이떡만들기.py
@@ -1,3 +1,20 @@
 n, m = map(int, input().split())
 g = list(map(int, input().split()))
+result = 0
+start = 0
+end = max(g)
 
+while start<=end:
+    total = 0
+    mid = (start+end)//2
+    
+    for i in g:
+        if i > mid:
+            total += i - mid
+    if total < m :
+        end = mid-1
+    else:
+        result = mid #최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록(중요) 
+        start = mid+1
+
+print(result)
