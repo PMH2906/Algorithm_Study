7d0044d0cf3780adf89dee15bc5f7e7733e5ee75
박미희 <mihee78952@naver.com>
2021-12-29 17:41:01 +0900

    가장큰수

---
+++ Programmers/가장큰수.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    #int형의 list를 map을 사용하여 string으로 치환한 뒤, list로 변환
+    numbers = list(map(str, numbers))
+    
+    #lambda x : x * 3은 num 인자 각각의 문자열을 3번 반복한다는 뜻
+    #num인자가 30이면 "303030" 뜻
+    #문자열 비교는 아스키코드 값으로 치환되어 정렬
+    numbers.sort(key = lambda x : x*3, reverse = True)
+    
+    #int로 변환한 뒤, str로 변환하는 이유는 "000"을 "0"으로 처리하기 위해
+    return str(int(''.join(numbers)))
+print(solution([3, 30, 34, 5, 9]))
