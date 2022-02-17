3612ae0e69a70d6b6a5c92ec8d65f0cefb13a3e9
박미희 <mihee78952@naver.com>
2022-02-18 00:06:40 +0900

    여행경로

--- Programmers/여행경로_ing.py
+++ Programmers/여행경로_ing.py
@@ -1,34 +1,43 @@
-from collections import deque
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+from collections import defaultdict
 def solution(tickets):
-    
-    visited=[0 for _ in range(len(tickets))]
-    def bfs(tickets,visited):
-        q = deque()
-        result = ['ICN']
-        check = 'ICN'
-        for i in range(len(tickets)):
-                if tickets[i][0] in check:
-                    q.append((tickets[i][0], tickets[i][1],i))
-        while q:
-            a,b,index = q.pop()
-            while q:
-                a_n, b_n,index_n = q.pop()
-                if b[0] > b_n[0]:
-                    a, b, index= a_n, b_n,index_n
-                elif b[0] == b_n[0]:
-                    if b[1] > b_n[1]:
-                        a, b, index= a_n, b_n,index_n
-                    elif b[1] == b_n[1]:
-                        if b[2] > b_n[2]:
-                            a, b, index= a_n, b_n,index_n     
-                            
-            visited[index] = 1
-            check = b
-            result.append(check)
-            for i in range(len(tickets)):
-                if tickets[i][0] in check and visited[i] == 0:
-                    q.append((tickets[i][0], tickets[i][1],i))
-        return result
-    answer = bfs(tickets, visited)
-            
+    answer = []
+    routes = defaultdict(list) #사전 기본값으로 빈 리스트 생성
+    for ticket in tickets:
+        routes[ticket[0]].append(ticket[1])
+
+    for key in routes.keys():
+        routes[key].sort(reverse=True) #해당 key의 value 내림차순 정렬
+
+    stack = ['ICN'] #출발지점
+    while stack:
+        tmp = stack[-1] #top stack
+
+        if not routes[tmp]: #해당 출발 지점의 도착 정보가 더 이상 없을 경우, 차례로 answer에 넣어줌 
+            answer.append(stack.pop()) 
+        else:
+            stack.append(routes[tmp].pop()) #해당 출발 지점의 도착 정보를 stack에 넣어줌
+    answer.reverse() #즉, stack의 역순이 여행 경로
+
     return answer
